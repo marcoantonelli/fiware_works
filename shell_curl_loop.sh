3ca8e00e4dc2e94b72d52824bb16f9324cb6da5e
marcoantonelli <info@marcoantonelli.com>
2015-11-21 13:02:56 +0100

    Update shell_curl_loop.sh

--- shell_curl_loop.sh
+++ shell_curl_loop.sh
@@ -7,5 +7,5 @@ do
   curl -v -H "Accept: application/json" -H "Content-type: application/json" -X PUT -d '{"value": "'"$i"'"}' 192.168.2.135:1026/v1/contextEntities/Watly_01/attributes/human_distance
   i=$((i + 1))
   #stop loop for 3 seconds
-  sleep 5
+  sleep 3
 done
